0e30817251f7e8a86682062003c69d08e912a3f2
Soňa Matějková <mat.sona53@gmail.com>
2021-01-10 21:02:20 +0100

    Index.jsx oprava

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.html';
 import { App } from './App/App.jsx';
+import './style.css';
 
-render(<App />, document.querySelector('#app'));
+render(
+  <>
+    <header></header>
+    <main>
+      <App />
+    </main>
+    <footer></footer>
+  </>,
+  document.querySelector('#app'),
+);
